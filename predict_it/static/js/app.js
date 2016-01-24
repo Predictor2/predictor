689a83cccb01f042a28c443fb3d92c04f38fc8cd
Tara Kreft <tara.kreft@gmail.com>
2016-01-23 17:12:36 -0800

    building out controllers and services

--- predict_it/static/js/app.js
+++ predict_it/static/js/app.js
@@ -1,68 +1,7 @@
-(function(){
-  var app = angular.module('predictor', ['ui.bootstrap', 'ngCookies', 'ngRoute']);
+(function (){
 
-    $interpolateProvider.startSymbol('{[{');
-    $interpolateProvider.endSymbol('}]}');
-    
-    app.run(function ($http, $cookies) {
-      $http.defaults.headers.common['X-CSRFToken'] = $cookies.csrftoken;
-    });
+  angular.module('app', ['app.predictions', 'ngRoute']);
 
-    app.config(function($routeProvider, $locationProvider){
-
-      $routeProvider
-
-      .when('/', {
-        templateUrl: 'templates/account/login.html'
-        // controller: 'AuthController'
-      })
-
-      .when('/login', {
-        templateUrl: 'templates/account/login.html'
-        // controller: 'AuthController'
-      })
-
-      .when('/signup', {
-        templateUrl: 'templates/account/signup.html'
-        // controller: 'AuthController'
-      })
-
-      .when('/logout', {
-        templateUrl: 'templates/account/logout.html'
-        // controller: 'AuthController'
-      })
-
-      .when('/home', {
-        templateUrl: 'templates/pages/home.html',
-        controller: 'MainController',
-        resolve: {
-                    posts: function (PostService) {
-                      return PostService.list();
-                    }
-                  }
-      })
-      
-      .when("/post/:id", {
-        templateUrl: "templates/pages/home.html",
-        controller: "MainController",
-        resolve: {
-                    post: function ($route, PostService) {
-                      var postId = $route.current.params.id;
-                      return PostService.get(postId);
-                    }
-                  }
-        })
-
-      .when('/about', {
-        templateUrl: 'templates/pages/about.html',
-        controller: 'MainController'
-      });
-
-    });
-
-    $locationProvider.html5Mode(true);
-    $routeProvider.otherwise('/login');
 
 })();
-
 
--- predict_it/static/js/controllers/MainController.js
+++
@@ -1,5 +0,0 @@
-angular.module('predictor').controller('MainController', function($scope) {
-
-  $scope.tagline = 'why arent you working you jerk'; 
-
-});
