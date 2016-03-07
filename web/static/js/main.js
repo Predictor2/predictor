57865e8607d87ee2a1dbea9fda23a4374cffc907
John Shiver <johneshiver@gmail.com>
2016-03-06 18:46:32 -0800

    more updates

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -3,7 +3,7 @@ var app = angular.module('predictor', [
   'ui.router'
 ]);
 
-app.constant('BASE_URL', 'http://192.168.99.100:8000/api/predictions/');
+app.constant('BASE_URL', 'http://192.168.99.105/api/predictions/');
 
 app.config(function($stateProvider, $urlRouterProvider){
   $stateProvider
